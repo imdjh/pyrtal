509303ab3772f2d3503b77ad18451ef28eadc6ec
Dye Jarhoo <20123225@student.shiep.edu.cn>
2015-04-29 07:02:28 +0800

    fixes

--- schema.sql
+++ schema.sql
@@ -1,13 +1,20 @@
 drop table if exists fileset;
-drop table if exists audit_log;
+drop table if exists donut_log;
+drop table if exists pick_log;
 
 create table fileset (
-    fid INTEGER primary key autocrement,
+    fid INTEGER primary key autoincrement,
     md5 TEXT not null,
     name TEXT not null
-)
-create table audit_log (
-    pid INTEGER primary key autocrement,
-    fin_time NUMERIC not null,
-    size_grade INTEGER not null
-)
+);
+create table donut_log (
+    did INTEGER primary key autoincrement,
+    bake_time NUMBERIC not null,
+    size_grade INTEGER not null,
+    ip TEXT not null
+);
+create table pick_log (
+    pid INTEGER primary key autoincrement,
+    pick_time NUMBERIC not null,
+    ip TEXT not null
+);
